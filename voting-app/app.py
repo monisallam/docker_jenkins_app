55cde9cc4fe360d11f495f553214f5c992e14186
monisallam <monisallam@gmail.com>
2016-06-16 18:15:01 +0000

    removed the volumes out of compose file, and add cats vs dogs back in

--- voting-app/app.py
+++ voting-app/app.py
@@ -6,7 +6,7 @@ import random
 import json
 
 option_a = os.getenv('OPTION_A', "Rats")
-option_b = os.getenv('OPTION_B', "Fats")
+option_b = os.getenv('OPTION_B', "fats")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
