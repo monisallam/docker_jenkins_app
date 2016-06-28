c90e4153b78e1a1f45a1751857b906bcd175e19e
monisallam@ <monisallam@gmail.com>
2016-06-28 15:14:33 +0000

    actually changed it now

--- pull_dtr/voting-app/app.py
+++ pull_dtr/voting-app/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Rats")
-option_b = os.getenv('OPTION_B', "Mice")
+option_a = os.getenv('OPTION_A', "Chocolate")
+option_b = os.getenv('OPTION_B', "Vanilla")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
